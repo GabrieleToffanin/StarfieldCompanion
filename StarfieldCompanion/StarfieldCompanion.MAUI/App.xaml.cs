96af4ce3ee9eba26f4561acffae31af6af82241a
Gabriele Toffanin <GabrieleToffanin@outlook.it>
2023-08-24 19:32:08 +0200

    init

---
+++ StarfieldCompanion/StarfieldCompanion.MAUI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace StarfieldCompanion.MAUI;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ StarfieldCompanion/StarfieldCompanion.MAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace StarfieldCompanion.MAUI;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
